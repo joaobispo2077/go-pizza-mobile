e40d9e04f650842c3204bffdf4eaf1d8f02cb94f
João Bispo <joaobispo2077@gmail.com>
2022-04-06 13:04:32 -0300

    chore: setup linters (.editorconfig, eslint, prettier)

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
-import { ExampleComponent } from '@components/example';
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+import { ExampleComponent } from "@components/example";
 
 export default function App() {
   return (
@@ -16,8 +17,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- src/components/example.tsx
+++ src/components/example.tsx
@@ -1,5 +1,5 @@
-import { Text } from "react-native"
+import { Text } from "react-native";
 
 export const ExampleComponent = () => {
-  return <Text>Eu sou um componente!</Text>
-}
+  return <Text>Eu sou um componente!</Text>;
+};
